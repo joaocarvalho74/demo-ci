4d4215134248914d93c4eb80c86fb6252cf5d66e
João Pedro <jocresende74@gmail.com>
2024-12-07 20:55:17 -0300

    Adicionando bug na função soma

--- src/test/java/br/ufmg/dcc/CalculadoraTest.java
+++ src/test/java/br/ufmg/dcc/CalculadoraTest.java
@@ -4,17 +4,26 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CalculadoraTest {
+  
   @Test
   public void testeSoma1() {
     Calculadora calc = new Calculadora();
     int resultadoEsperado = 5;
     int resultadoRetornado = calc.soma(2,3);
-    assertEquals(resultadoEsperado, resultadoRetornado);
+    assertEquals("O resultado da soma deveria ser 5", resultadoEsperado, resultadoRetornado); // Mensagem de erro personalizada
   }
 
   @Test
   public void testeSoma2() {
     Calculadora calc = new Calculadora();
-    assertEquals(10, calc.soma(4,6));
+    assertEquals("O resultado da soma deveria ser 10", 10, calc.soma(4,6)); // Mensagem de erro personalizada
+  }
+
+  // Novo teste que verifica o bug introduzido
+  @Test
+  public void testeSomaComBug() {
+    Calculadora calc = new Calculadora();
+    // O cálculo correto seria 2 + 3 = 5, mas devido ao bug, será 6
+    assertEquals("O resultado da soma com bug deveria ser 6", 6, calc.soma(2,3)); 
   }
 }
